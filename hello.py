c50285c6e0ac85567473984e3659f59be8d0f643
brunorocha-dev <brunorocha.freitass@hotmail.com>
2024-01-04 10:01:34 -0300

    adding multi-line comments

--- hello.py
+++ hello.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # shebang -> Sistema unix, comentario especial
 
+"""Hello World Multi Linhas
+
+Dependendo da linguagem configurada do ambiente o programa exibe a mensagem 
+correspondente.
+
+USAGE:
+Tenha a variável LANG devidamente configurada, exemplo:
+
+    export LANG=pt_BR
+
+RUN:
+
+    python3 hello.py
+    ou
+    ./hello.py
+
+"""
 print("Hello Word".upper())
 print("Bruno Rocha")
