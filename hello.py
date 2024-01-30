27e87200164b41dbcfe4321db6ab70ca064554a3
brunorocha-dev <brunorocha.freitass@hotmail.com>
2024-01-29 23:03:52 -0300

    adding dict

--- hello.py
+++ hello.py
@@ -19,22 +19,20 @@ RUN:
 
 """
 # METADADOS, variáveis de distribuíções com (Dunder). 
-__version__= "0.0.1"
+__version__= "0.1.2"
 __author__= "Bruno Rocha"
 __license__= "unlicense"
 
 import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde!"
-
-print(msg)
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Bom dia, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour Monde!"
+}
+# Ordem de complexidade O(N)
+print(msg[current_language])
